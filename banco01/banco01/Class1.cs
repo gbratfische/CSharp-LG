5248ebc6c1026968d695ca15a5e9fb0fe6457e23
Luís Guilherme Bratfische <gbratfische@gmail.com>
2024-09-18 19:20:16 -0300

    Add files via upload

---
+++ banco01/banco01/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banco01
+{
+    internal class ContaBancaria
+    {
+        string numeroConta;
+        float saldo;
+
+        public ContaBancaria()
+        {
+            Random rnd = new Random();
+            numeroConta = rnd.Next(100).ToString();
+            saldo = 0;
+        }
+        public void Depositar(float valor)
+        {
+            saldo += valor;
+        }
+        public bool Sacar(float valor)
+        {
+            if (valor <= saldo)
+            {
+                saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public float getSaldo() 
+        { 
+            return saldo; 
+        }
+        public override string ToString() 
+        { 
+            return $"Conta: {numeroConta}\nSaldo: {saldo}";
+        }
+    }
+}
---
+++ banco01/banco01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banco01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcao;
+            float valor = 0;
+            ContaBancaria contaBancaria = new ContaBancaria();
+
+            do
+            {
+            Console.WriteLine("Informe a ação desejada 1-Depositar\n2-Sacar\n3-Ver Saldo\n0-Sair");
+            opcao=int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        contaBancaria.Depositar(10);
+                        break;
+                    case 2:
+                        if (contaBancaria.Sacar(5))
+                        {
+                            Console.WriteLine("Saque efetuado!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Saldo insuficiente;");
+                        }
+                        break;
+                    case 3:
+                        Console.WriteLine(contaBancaria.ToString());
+                        break;
+                    case 0:
+                        Console.WriteLine("Fim do programa");
+                        break;
+                    default:
+                        Console.WriteLine(("Opção incorreta!"));
+                        break;
+                }
+            } while (opcao != 0);
+            Console.ReadKey();
+        }
+    }
+}
