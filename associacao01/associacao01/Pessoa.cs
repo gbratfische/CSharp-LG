9023d3c7084f76a5a5315f1b13c608e3b4e0b7ab
Luís Guilherme Bratfische <gbratfische@gmail.com>
2024-09-18 21:46:24 -0300

    Add files via upload

---
+++ associacao01/associacao01/Pessoa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace associacao01
+{
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+        public Endereco Endereco { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoa(string nome, string telefone, Endereco endereco, int idade)
+        {
+            Nome = nome;
+            Telefone = telefone;
+            Endereco = endereco;
+            Idade = idade;
+        }
+
+        public override string ToString()
+        {
+            return $"Dados da pessoa:\nNome: {Nome}"+
+                   $"\nTelefone: {Telefone}"+
+                   $"\nIdade: {Idade}"+
+                   $"\n{Endereco.ToString()}";
+        }
+    }
+}
