252e34fbe4907685103b09741473f5a3d95f62d9
Luís Guilherme Bratfische <gbratfische@gmail.com>
2024-09-25 20:00:36 -0300

    Add files via upload

---
+++ associacao01/associacao01/PessoaFisica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace associacao01
+{
+    internal class PessoaFisica:Pessoa
+    {
+        public string CPF { get; set; }
+
+        public PessoaFisica(string nome, string telefone, Endereco endereco, int idade, string cPF): base(nome, telefone, endereco, idade)
+        {
+            //:base retorna os valores de herança da classe Pessoa
+            CPF = cPF;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + $"\nCPF: {CPF}";
+        }
+    }
+}
---
+++ associacao01/associacao01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace associacao01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Endereco endereco = new Endereco("Precide Scarpino", "Rua", "Araraquara", "São Paulo", "Jd Santa Clara", 14811380, 126, "Fatec Araraquara");
+            
+            Pessoa pessoa = new Pessoa("Guilherme", "190", new Endereco("Precide Scarpino", "Rua", "Araraquara", "São Paulo", "Jd Santa Clara", 14811380, 126, "Fatec Araraquara"), 29);
+            Console.WriteLine(pessoa.ToString());
+
+            PessoaFisica pessoaFisica = new PessoaFisica("Guilherme", "190", new Endereco("Precide Scarpino", "Rua", "Araraquara", "São Paulo", "Jd Santa Clara", 14811380, 126, "Fatec Araraquara"), 29, "12345678901");
+            Console.WriteLine();
+            Console.WriteLine(pessoaFisica.ToString());
+            Console.ReadKey();
+        }
+    }
+}
